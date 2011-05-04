4cb0afca935b9d17dcf15adf67a7c2bd600f56ff
patriotsfan <patriotsfan@a3e9d743-0113-b1c5-0c61-4476407d8246>
2011-05-04 19:16:44 +0000

    rl_common package

---
+++ trunk/stacks/reinforcement_learning/rl_common/include/rl_common/ExperienceFile.hh
@@ -0,0 +1,29 @@
+#ifndef _EXPFILE_HH_
+#define _EXPFILE_HH_
+
+#include "Random.h"
+#include "core.hh"
+
+#include <vector>
+
+
+class ExperienceFile {
+public:
+  /** Standard constructor
+      */
+  ExperienceFile();
+  
+  ~ExperienceFile();
+
+  void initFile(const char* filename, int nfeats);
+  void saveExperience(experience e);
+  void printExperience(experience e);
+  std::vector<experience> loadExperiences(const char* filename);
+  void closeFile();
+
+  ofstream vectorFile;
+  int expNum;
+
+};
+
+#endif
